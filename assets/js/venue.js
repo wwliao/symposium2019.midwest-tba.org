26b1372dc2f33561c56e75a0bfd4add06eb71da3
Wen-Wei Liao <wen-wei.liao@wustl.edu>
2018-06-09 23:41:27 -0500

    Add google map

---
+++ assets/js/venue.js
@@ -0,0 +1,29 @@
+// Google Maps
+function initMap() {
+    var center = {lat: 41.7916469, lng: -87.6034081};
+    var map = new google.maps.Map(document.getElementById('venue'), {
+        zoom: 18,
+        center: center,
+        scrollwheel: false,
+    });
+
+    // Marker setting for Knapp Center
+    var centerMarker = new google.maps.Marker({
+        position: center,
+        map: map,
+        title: "MTBA Symposium 2018 (Knapp Center for Biomedical Discovery)"
+        // label: 'ABC'
+    });
+    var centerInfoWindow = new google.maps.InfoWindow({
+        content: "<div class='map-info'>" +
+            "<h4>MTBA Symposium 2018</h4>" +
+            "<p>Auditorium (Room 1103)<br>" +
+            "Knapp Center for Biomedical Discovery<br>" +
+            "900 E 57th St, Chicago, IL 60637<p>" +
+            "</div>"
+    });
+    centerMarker.addListener('click', function () {
+        centerInfoWindow.open(map, centerMarker);
+    });
+
+}
