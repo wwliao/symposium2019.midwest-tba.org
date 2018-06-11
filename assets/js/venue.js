7fa0f6a1f3584bc1fd776a50581a9c9ae289ab6e
Wen-Wei Liao <wen-wei.liao@wustl.edu>
2018-06-10 23:59:35 -0500

    Change styles of google maps

--- assets/js/venue.js
+++ assets/js/venue.js
@@ -2,9 +2,10 @@
 function initMap() {
     var center = {lat: 41.7916469, lng: -87.6034081};
     var map = new google.maps.Map(document.getElementById('venue'), {
-        zoom: 15,
+        zoom: 14,
         center: center,
         scrollwheel: false,
+        styles: [{"featureType":"administrative","elementType":"all","stylers":[{"visibility":"on"},{"lightness":33}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2e5d4"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#c5dac6"}]},{"featureType":"poi.park","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":20}]},{"featureType":"road","elementType":"all","stylers":[{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#c5c6c6"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#e4d7c6"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#fbfaf7"}]},{"featureType":"water","elementType":"all","stylers":[{"visibility":"on"},{"color":"#acbcc9"}]}]
     });
 
     // Marker setting for Knapp Center
